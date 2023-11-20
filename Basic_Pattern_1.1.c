6cca584181338bd52891cf7cb09ec721be13d9d1
Sandeep Reddy <88756524+Sandeep-Reddy-18@users.noreply.github.com>
2023-11-20 15:16:28 +0530

    #include<stdio.h> int main() {     int n,r,c;     scanf("%d", &n);     for(c=1; c<=n; c++)     {         for(r=1; r<=n; r++)         {             printf("* ");         }         printf(" ");     } }

---
+++ Basic_Pattern_1.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,c;
+    scanf("%d", &n);
+    for(c=1; c<=n; c++)
+    {
+        for(r=1; r<=n; r++)
+        {
+            printf("* ");
+        }
+        printf("
+");
+    }
+}
