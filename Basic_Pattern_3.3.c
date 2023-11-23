ed68210a9195f6335ae03f2911309b6537186b14
Sandeep Reddy <88756524+Sandeep-Reddy-18@users.noreply.github.com>
2023-11-23 10:04:29 +0530

    #include<stdio.h> int main() {     int n,r,c;     scanf("%d", &n);     int temp=n;     for(c=1; c<=n; c++)     {         for(r=1; r<=temp; r++) printf("%d ", r);         printf(" ");         temp--;     } }

--- Basic_Pattern_3.3.c
+++ Basic_Pattern_3.3.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int n, r,c;
+    int n,r,c;
     scanf("%d", &n);
     int temp=n;
-    for(c=1; c<=n;c++)
+    for(c=1; c<=n; c++)
     {
         for(r=1; r<=temp; r++) printf("%d ", r);
         printf("
